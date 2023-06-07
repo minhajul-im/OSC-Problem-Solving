0bb1cf1e814d9681547f011ebb1f41371984c565
minhajul-im <minhajul.cpp@gmail.com>
2023-06-07 15:39:23 +0600

    fixes issue-4

---
+++ 26. Remove Duplicates from Sorted Array.js
@@ -0,0 +1,14 @@
+// 26. Remove Duplicates from Sorted Array
+
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+
+  return nums.length;
+};
+
+let nums = [1, 1, 2];
